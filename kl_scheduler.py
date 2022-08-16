kl sheudler fix?

kl_scheduler.py
@@ -22,11 +22,11 @@ class KLScheduler:
             for k in range(number_of_splits):
                 kl_multiplier.append((4**(model.number_of_scales-1-i))/number_of_splits)
 
-            number_of_splits = max(model.min_splits, number_of_splits // model.exponential_scaling)
+            number_of_splits = min(model.min_splits, number_of_splits // model.exponential_scaling)
         
         kl_multiplier.reverse()
         self.kl_multiplier = torch.FloatTensor(kl_multiplier).unsqueeze(1)
-        self.kl_multiplier = self.kl_multiplier/torch.max(self.kl_multiplier)
+        self.kl_multiplier = self.kl_multiplier/torch.min(self.kl_multiplier)
         self.kl_multiplier = self.kl_multiplier.to(device)
 
     # for warm up
@@ -47,7 +47,9 @@ class KLScheduler:
             # average kl_loss for this group across batches
             kl_coeff_i = torch.mean(kl_coeff_i, dim=1, keepdim=True) + 0.01
 
-            kl_coeff_i = kl_coeff_i * self.kl_multiplier * torch.sum(kl_coeff_i)
+
+            # TODO check if should divide or multiplie
+            kl_coeff_i = kl_coeff_i / self.kl_multiplier * torch.sum(kl_coeff_i)
             kl_coeff_i = kl_coeff_i / torch.mean(kl_coeff_i, dim=0, keepdim=True)
 
 